68d37b07294d387a9b59638b3c4c800df5d66d33
Anxo Abuelo Veira <anxo.abuelo@udc.es>
2023-10-03 21:56:55 +0200

    Rango dinámico y Ecualización terminada?

--- prueba.py
+++ prueba.py
@@ -2,6 +2,30 @@ import numpy as np
 import cv2 as cv 
 import matplotlib.pyplot as plt
 
+
+def dibujarHist(inHist,outHist):
+    plt.figure(figsize=(12,6))
+
+    plt.subplot(1,2,1)
+    plt.title('Histograma')
+    plt.plot(inHist)
+    plt.xlim([0,256])
+    plt.xlabel('Valor del píxel')
+    plt.ylabel('Frecuencia')
+
+    plt.subplot(1,2,2)
+    plt.title('Histograma 2')
+    plt.plot(outHist)
+    plt.xlim([0,256])
+    plt.xlabel('Valor del píxel')
+    plt.ylabel('Frecuencia')
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+#Alteración del rango dinámico
 def adjustIntensity(inImage,inRange=[],outRange=[0,1]):
    minImage = np.min(inImage)
    maxImage = np.max(inImage)
@@ -9,9 +33,6 @@ def adjustIntensity(inImage,inRange=[],outRange=[0,1]):
    height,width = inImage.shape[:2] 
 
    outImage = np.zeros((height,width))
-
-   print(height)
-   print(width)
 
    if not inRange:
     min = minImage
@@ -22,27 +43,40 @@ def adjustIntensity(inImage,inRange=[],outRange=[0,1]):
 
    for i in range(height):
     for j in range(width):
-        outImage[i,j] = min + ((max - min) * ((inImage[i,j] - minImage)/(maxImage-minImage)))
+        outImage[i,j] = outRange[0] + (((outRange[1] - outRange[0]) * (inImage[i,j] - min))/(max-min))
    
    return outImage 
 
+#Ecualizacuón de histograma
+def equalizeIntensity(inImage,nBins=256):
+    height,width = inImage.shape[:2]
+
+    hist = cv.calcHist([inImage],[0],None,[nBins],[0,256])
+    hist_acum = np.cumsum(hist)
+
+    t = ((hist_acum/(height*width))*255)
+
+    outImage = t[inImage]
+
+    return outImage
+
 def main():
-    inImage = cv.imread('plantaosucra.jpg',cv.IMREAD_GRAYSCALE)
+    inImage = cv.imread('plantaoscura.jpg',cv.IMREAD_GRAYSCALE)
 
-    hist = cv.calcHist([inImage],[0],None,[256],[0,256])
+    inImageNorm = inImage/255.0
 
-    plt.plot(hist)
-    plt.title('Histograma normal')
+    hist = cv.calcHist([np.uint8(inImageNorm * 255)],[0],None,[256],[0,256])
 
-    outImage = adjustIntensity(inImage,[0,255],outRange=[0,1])
+    outImage = adjustIntensity(inImageNorm,[],[0,1])
+    outImage2 = equalizeIntensity(inImage)
 
-    # hist2 = cv.calcHist([outImage],[0],None,[256],[0,256])
+    histdinam = cv.calcHist([np.uint8(outImage * 255)],[0],None,[256],[0,256])
+    histequal = cv.calcHist([np.uint8(outImage2 * 255)],[0],None,[256],[0,256])
 
-    # plt.plot(hist2)
-    # plt.title('Histograma cambios')
-    # plt.show()
+    dibujarHist(hist,histequal)
 
-    cv.imwrite('imagen_salida.png',outImage)
+    cv.imwrite('imagen_rdinamico.jpg',np.uint8(outImage * 255))
+    cv.imwrite('imagen_ecualizada.jpg',outImage2)
 
 if __name__ == "__main__":
     main()
